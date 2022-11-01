9e3eca72db799f7bd61cb37ceab93f080963a3b5
jlorion <orionjl1106@gmail.com>
2022-11-01 23:07:25 +0800

    full crud complete

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const Listing = require('./model/listings');
 
+// for put, patch and delete methods
+var methodOverride = require('method-override'); 
+app.use(methodOverride('_method'));
+
+// layout
 const layout = require('express-ejs-layouts');
 app.set('layout', 'layouts/layout');
 app.use(layout);
 
-
+// some useful add-ons 
 app.set('view engine', 'ejs');
-app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+// database connection
+mongoose.connect('mongodb://localhost:27017/ListingsApp')
+    .then(() => {
+        console.log('connection open')
+    })
+    .catch(err => {
+        console.log("Error");
+        console.log(err);
+    })
+
 
 // path for acces views
 const path = require('path');
@@ -22,19 +41,44 @@ app.get('/', (req, res) => {
 })
 
 
-app.get('/listings', (req, res)=> {
-    res.render('listings');
+app.get('/listings', async (req, res)=> {
+    const listings = await Listing.find({});
+    res.render('./listings/listings', {listings});
+})
+
+app.get('/listings/new', (req, res) => {
+    res.render('./listings/new');
+})
+
+app.post('/listings', async (req, res) => {
+    const newListing = new Listing(req.body);
+    await newListing.save();
+    res.redirect('/listings');
+})
+
+
+
+app.get('/listings/:id', async (req, res)=> {
+    const {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render('./listings/show', {listing});
+})
+
+app.get('/listings/:id/edit', async (req, res)=> {
+    const {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render('./listings/edit', {listing});
+})
+
+app.delete('/listings/:id', async (req, res)=>{
+    const {id} = req.params;
+    await Listing.deleteOne({_id: id});
+    res.redirect('/listings');
 })
 
 app.get('/about', (req, res) => {
     res.render('about');
 })
-
-
-
-
-
-
 
 
 app.listen(3000, () => {
